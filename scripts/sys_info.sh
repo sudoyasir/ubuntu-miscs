192ed3afc0733c15a98b2b98b284585b428fca39
Yasir Nawaz <94890956+yasir2002@users.noreply.github.com>
2023-11-04 17:44:06 +0500

    Add sys_info.sh

---
+++ scripts/sys_info.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Operating System: $(uname -a)"
+echo "Kernel Version: $(uname -r)"
+echo "CPU Information: $(lscpu)"
+echo "Memory Information: $(free -m)"
+echo "Disk Usage: $(df -h)"
+
+# This script will print system information.
