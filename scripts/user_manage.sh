4154bb448a51b04ec2f9fdc4cefbcfabf2e2546a
Yasir Nawaz <94890956+yasir2002@users.noreply.github.com>
2023-11-04 17:45:57 +0500

    Added user_manage.sh in scripts/

---
+++ scripts/user_manage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+action=$1
+username=$2
+
+if [ "$action" = "add" ]; then
+    sudo useradd -m $username
+    sudo passwd $username
+    echo "User $username added."
+elif [ "$action" = "remove" ]; then
+    sudo userdel -r $username
+    echo "User $username removed."
+elif [ "$action" = "modify" ]; then
+    sudo usermod -aG sudo $username
+    echo "User $username modified."
+else
+    echo "Invalid action. Usage: user_management.sh add|remove|modify username"
+fi
+
+# This script will add, remove, or modify a user.
