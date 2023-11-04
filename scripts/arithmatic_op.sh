04f959889a2604db84c09026afede1def06d15c2
Yasir Nawaz <94890956+yasir2002@users.noreply.github.com>
2023-11-04 17:42:10 +0500

    Refactor code for improved readability and performance.

---
+++ scripts/arithmatic_op.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "Enter first number: "
+read num1
+echo "Enter second number: "
+read num2
+
+sum=$((num1 + num2))
+echo "Sum: $sum"
+
+difference=$((num1 - num2))
+echo "Difference: $difference"
+
+product=$((num1 * num2))
+echo "Product: $product"
+
+if [ $num2 -ne 0 ]
+then
+    division=$(awk "BEGIN {print $num1/$num2}")
+    echo "Division: $division"
+else
+    echo "Cannot divide by zero."
+fi
+
+# This script will take two numbers as input and perform arithmetic operations on them.
---
+++ scripts/user_info.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Username: $USER"
+echo "Home directory: $HOME"
+echo "User ID: $UID"
+echo "Groups: $(groups)"
